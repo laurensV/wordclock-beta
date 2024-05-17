dd2a7048b07c6b785a1a6611f057eeb61f032fb2
Laurens Verspeek <l.verspeek@gmail.com>
2024-05-17 20:34:36 +0200

    switch to beta

--- wordclock.ino
+++ wordclock.ino
@@ -350,7 +350,7 @@ void setupOTA() {
   ArduinoOTA.setHostname(HOSTNAME);
   ArduinoOTA.begin();
   FOTA = new esp8266FOTA("wordclock", VERSION, fileSystemVersion);
-  FOTA->checkURL = "https://raw.githubusercontent.com/laurensV/wordclock/main/firmware/version.json";
+  FOTA->checkURL = "https://raw.githubusercontent.com/laurensV/wordclock-beta/main/firmware/version.json";
   FOTA->onProgress(onUpdateProgress);
   FOTA->onEnd(onUpdateFinished);
   ArduinoOTA.onProgress(onUpdateProgress);
